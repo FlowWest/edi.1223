a269165b10f1ee5c5fc00394c46375d889dc7eed
MaddeeRubenson <rubenson.m@gmail.com>
2022-09-27 14:43:12 -0700

    adding all yuba hallwood data and processed snorkel data. XML process WIP

---
+++ clean_data.R
@@ -0,0 +1,77 @@
+library(tidyverse)
+library(readxl)
+library(lubridate)
+
+list.files('data-raw/')
+
+raw_snorkel_all <- read_csv('data-raw/snorkel_all.csv') %>% glimpse
+
+snorkel_all <- raw_snorkel_all %>%
+  mutate(Date = as_date(Date, format = "%m/%d/%Y"),
+         StartTime =  as_datetime(StartTime, format = "%m/%d/%Y %H:%M"),
+         EndTime = as_datetime(EndTime, format = "%m/%d/%Y %H:%M"),
+         Species_Code = ifelse(Species_Code == "Unknown Larval", "LARVAL", Species_Code)) %>% glimpse
+
+unique(snorkel_all$ProjectID)
+
+# plot numeric variables--------------------------------------------------------
+
+
+ggplot(data = snorkel_all, aes(x = Date, y = Temperature_C)) + 
+  geom_point()
+
+ggplot(data = snorkel_all, aes(x = Date, y = DO_mgL)) + 
+  geom_point()
+
+ggplot(data = snorkel_all, aes(x = Date, y = Turbidity_NTU)) + 
+  geom_point()
+
+ggplot(data = snorkel_all, aes(x = Date, y = Depth)) + 
+  geom_point()
+
+ggplot(data = snorkel_all, aes(x = Date, y = Velocity1)) + 
+  geom_point() 
+
+ggplot(data = snorkel_all, aes(x = Date, y = Velocity2)) + 
+  geom_point() 
+
+ggplot(data = snorkel_all, aes(x = Date, y = Visibility)) + 
+  geom_point() 
+
+ggplot(data = snorkel_all, aes(x = Date, y = Count)) + 
+  geom_point() 
+
+ggplot(data = snorkel_all, aes(x = Date, y = xsVelocity_1)) + 
+  geom_point() 
+
+ggplot(data = snorkel_all, aes(x = as.numeric(XSDVTransectNo))) + 
+  geom_histogram() 
+
+
+# explore character variables  --------------------------------------------
+
+table(snorkel_all$TransectID, useNA = "ifany")
+length(unique(snorkel_all$TransectID))
+
+unique(snorkel_all$Notes, useNA = "ifany")
+
+table(snorkel_all$Bank, useNA = "ifany")
+
+table(snorkel_all$SingleSnorkeler, useNA = "ifany")
+
+unique(snorkel_all$ObsID, useNA = "ifany")
+
+unique(snorkel_all$AreaCode, useNA = "ifany")
+
+unique(snorkel_all$FishObsID, useNA = "ifany")
+
+# changed unknown larval to "LARVAL". see above. 
+table(snorkel_all$Species_Code, useNA = "ifany")
+
+unique(snorkel_all$Comment, useNA = "ifany")
+
+
+# write clean csv  --------------------------------------------------------
+
+write_csv(snorkel_all, 'data/snorkel_all.csv')
+
---
+++ make_metadata_xml.R
@@ -0,0 +1,61 @@
+library(EMLaide)
+library(tidyverse)
+
+# Load in all the documents
+datatable_metadata <- tibble(filepath = c("data/snorkel_all.csv"),
+                             attribute_info = c("data-raw/metadata_snorkel.xlsx"),
+                             datatable_description = c("Snorkel Data"))
+
+
+# TODO Check warnings when reading in excel sheets
+excel_path <- "data-raw/metadata_snorkel.xlsx"
+sheets <- readxl::excel_sheets(excel_path)
+metadata <- lapply(sheets, function(x) readxl::read_excel(excel_path, sheet = x))
+names(metadata) <- sheets
+
+abstract_docx <- "data-raw/abstract_final.docx"
+methods_docx <- "data-raw/methods.docx"
+
+# edi_number <- reserve_edi_id(user_id = Sys.getenv("EDI_user"), password = Sys.getenv("EDI_password"))
+
+
+edi_number = "edi.996.1"
+
+dataset <- list() %>%
+  add_pub_date() %>%
+  add_title(metadata$title) %>%
+  add_personnel(metadata$personnel) %>%
+  add_keyword_set(metadata$keyword_set) %>%
+  add_abstract(abstract_docx) %>%
+  add_license(metadata$license) %>%
+  add_method(methods_docx) %>%
+  add_maintenance(metadata$maintenance) %>%
+  add_project(metadata$funding) %>%
+  add_coverage(metadata$coverage, metadata$taxonomic_coverage) %>%
+  add_datatable(datatable_metadata)
+
+custom_units <- data.frame(id = c("microseimens per centimeter", "parts per thousand", "percent saturation", "pH",
+                                  "Nephelometric Turbidity unit", "rotation start", "rotation end", "rank score"),
+                           unitType = c("density", "density", "dimensionless", "dimensionless", "dimensionless",
+                                        "dimensionless", "dimensionless", "dimensionless"),
+                           parentSI = c(NA, NA, NA, NA, NA, NA, NA, NA),
+                           multiplierToSI = c("siemensPerCentimeter", NA, NA, NA, NA, NA, NA, NA),
+                           description = c("Number of microsiemens per centimeter, conductivity measurement",
+                                           "Salinity measure of parts per thousand",
+                                           "Percent saturation of disolved oxygen",
+                                           "Measure of potential hydrogen",
+                                           "Turbidity Units",
+                                           "The start number of the flowmeter rotor",
+                                           "The end number of the flowmeter rotor",
+                                           "Subjective rank score of zooplankton density"))
+
+unitList <- EML::set_unitList(custom_units)
+eml <- list(packageId = edi_number,
+            system = "EDI",
+            access = add_access(),
+            dataset = dataset,
+            additionalMetadata = list(metadata = list(
+              unitList = unitList)))
+
+EML::write_eml(eml, "edi.996.1.xml")
+EML::eml_validate("edi.996.1.xml")
