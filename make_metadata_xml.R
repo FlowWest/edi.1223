8399f813b04005d4f2694336c8dc1c10a6946317
ErinCain <ecain@flowwest.com>
2022-09-28 16:20:37 -0700

    Adds in zip file to EML as an other entity

--- make_metadata_xml.R
+++ make_metadata_xml.R
@@ -1,13 +1,19 @@
 library(EMLaide)
 library(tidyverse)
 
-# Load in all the documents
+# Load in tabular data 
 datatable_metadata <- tibble(filepath = c("data/snorkel_all.csv"),
                              attribute_info = c("data-raw/metadata_snorkel.xlsx"),
                              datatable_description = c("Snorkel Data"))
 
+# Prep Other Entity metadata
+other_entity_metadata <- list("file_name" = "2014_2022_HW_snorkel_final.zip",
+                              "file_description" = "A zipped folder containing vector shapefiles associated with this dataset",
+                              "file_type" = "zipped",
+                              "physical" = create_physical("data-raw/2014_2022_HW_snorkel_final.zip"))
 
-# TODO Check warnings when reading in excel sheets
+
+# Check warnings when reading in excel sheets
 excel_path <- "data-raw/metadata_snorkel.xlsx"
 sheets <- readxl::excel_sheets(excel_path)
 metadata <- lapply(sheets, function(x) readxl::read_excel(excel_path, sheet = x))
@@ -16,9 +22,9 @@ names(metadata) <- sheets
 abstract_docx <- "data-raw/abstract_final.docx"
 methods_docx <- "data-raw/methods.docx"
 
-# edi_number <- reserve_edi_id(user_id = Sys.getenv("EDI_user"), password = Sys.getenv("EDI_password"))
+# edi_number <- reserve_edi_id(user_id = Sys.getenv("edi_user_id"), password = Sys.getenv("edi_password"))
 
-edi_number = "edi.1221.1"
+edi_number = "edi.1223.1"
 
 dataset <- list() %>%
   add_pub_date() %>%
@@ -31,7 +37,8 @@ dataset <- list() %>%
   add_maintenance(metadata$maintenance) %>%
   add_project(metadata$funding) %>%
   add_coverage(metadata$coverage, metadata$taxonomic_coverage) %>%
-  add_datatable(datatable_metadata)
+  add_datatable(datatable_metadata) %>% 
+  add_other_entity(other_entity_metadata)
 
 custom_units <- data.frame(id = c("nephelometric turbidity units", "fish"),
                            unitType = c("dimensionless", "dimensionless"),
